0646dfc135d933a7713776af8059cb0a532f7eec
migzursan <migzursan@gmail.com>
2025-11-25 15:25:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/guardian-angel/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
